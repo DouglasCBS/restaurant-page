396afc5e855bda8ba2cfd9915283d0a4b49020c4
DouglasCBS <douglascesar12@gmail.com>
2023-07-27 22:05:41 -0300

    Finish basic pageLoad styling

--- src/pages/pageLoad.js
+++ src/pages/pageLoad.js
@@ -3,14 +3,18 @@ const pageLoad = function (){
     const header = document.createElement("header");
     const main = document.createElement("section");
     const footer = document.createElement("footer");
+    const div = document.createElement("div");
+    div.classList.add("main");
 
-    header.textContent = "header";
-    main.textContent = "main";
-    footer.textContent = "footer";
+    header.textContent = "Spastalleto";
+    div.textContent = "What do you call something that tastes like pasta, looks like pasta but isn’t pasta? An impasta."
+    /* main.textContent = "main"; */
+    footer.textContent = "Photo by Pixabay: https://www.pexels.com/photo/yellow-pasta-beside-onions-47306/";
 
     content.appendChild(header);
     content.appendChild(main);
     content.appendChild(footer);
+    main.appendChild(div);
 }
 
 export default pageLoad;
