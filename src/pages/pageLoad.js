ec2253949b5a1cf13d90be83093fdcbf208e02c5
DouglasCBS <douglascesar12@gmail.com>
2023-07-29 17:56:17 -0300

    add nav with home, menu and contact buttons

--- src/pages/pageLoad.js
+++ src/pages/pageLoad.js
@@ -8,13 +8,27 @@ const pageLoad = function (){
 
     header.textContent = "Spastalleto";
     div.textContent = "What do you call something that tastes like pasta, looks like pasta but isn’t pasta? An impasta."
-    /* main.textContent = "main"; */
-    footer.innerHTML = '<p>Image by <a href="https://pixabay.com/users/pfüderi-199315/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2364221">Pfüderi</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2364221">Pixabay</a><p>';
+    footer.innerHTML = '<p>Background image by <a href="https://pixabay.com/users/pfüderi-199315/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2364221">Pfüderi</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2364221">Pixabay</a><p>';
 
     content.appendChild(header);
     content.appendChild(main);
     content.appendChild(footer);
     main.appendChild(div);
+
+    const home = document.createElement("button");
+    const menu = document.createElement("button");
+    const contact = document.createElement("button");
+
+    home.textContent = "Home";
+    menu.textContent = "Menu";
+    contact.textContent = "Contact";
+
+    const nav = document.createElement("nav");
+    header.appendChild(nav);
+
+    nav.appendChild(home);
+    nav.appendChild(menu);
+    nav.appendChild(contact);
 }
 
 export default pageLoad;
