ae2edf3ff33b0250b06817c16f6fba80736a68de
DouglasCBS <douglascesar12@gmail.com>
2023-07-30 18:17:02 -0300

    Add content and styling to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,8 @@
 const contact = function () {
     const div = document.createElement("div");
     document.querySelector("section").appendChild(div);
+    div.classList.add("contactDiv");
+    div.innerHTML = "<h1>Contact us</h1> <ul><li>+12 012 91234-5678</li> <li>spastalletoemail@realemail.com</li> <li>Piazza del Duomo, 56126 Pisa PI, Italy</li></ul>";
 }
 
 export default contact;
